3fae347c21d2b9778ff759224ce418c5fa8531f8
noearobles <roblesnoe1030@gmail.com>
2023-11-04 21:42:21 -0500

    adjusted footer banner

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import { urlFor } from '../lib/client';
+import { urlFor } from "../lib/client";
 
-const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image } }) => {
+const FooterBanner = ({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+  },
+}) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
-        <div className="left">
+        {/* <div className="left">
           <p>{discount}</p>
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
-        </div>
+        </div> */}
         <div className="right">
           <p>{smallText}</p>
           <h3>{midText}</h3>
@@ -27,7 +40,7 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
         /> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterBanner
+export default FooterBanner;
