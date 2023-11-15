f2f0bd3654f3f2138f71f199dd56163f2af7be6f
noearobles <roblesnoe1030@gmail.com>
2023-11-15 14:16:56 -0600

    modified footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { AiFillInstagram, AiOutlineFacebook, AiOutlineTwitter} from 'react-icons/ai';
+import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2023 HypeVillage All rights reserverd<br></br>&#169; Developed by<a style={{textDecoration:"underline"}} href="https://noearobles.github.io/noesPortfolio"> Noe Robles</a></p>
+      <p>
+        2023 HypeVillage<br></br>&#169; Developed by
+        <a
+          style={{ textDecoration: "underline" }}
+          href="https://noearobles.github.io/noesPortfolio"
+        >
+          {" "}
+          Noe Robles
+        </a>
+      </p>
       <p className="icons">
-        <a href="https://www.instagram.com/hypevillagetx/"><AiFillInstagram /></a>
-        <a href="https://www.facebook.com/HypeVillageTX/">  <AiOutlineFacebook /></a>    
+        <a href="https://www.instagram.com/hypevillagetx/">
+          <AiFillInstagram />
+        </a>
+        <a href="https://www.facebook.com/HypeVillageTX/">
+          {" "}
+          <AiOutlineFacebook />
+        </a>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
