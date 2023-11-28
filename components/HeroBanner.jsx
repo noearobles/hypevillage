b1ba23b4da7a347cecfc6d1d99a8378a4bceda79
noearobles <roblesnoe1030@gmail.com>
2023-11-28 12:30:58 -0600

    modified hero banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi"; // Import arrows from react-icons
 import { urlFor } from "../lib/client";
-
 const HeroBanner = ({
   heroBanner: {
     discount,
@@ -17,30 +16,70 @@ const HeroBanner = ({
     image,
   },
 }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const galleryImages = [
+    "footwear.jpg",
+    "hvTee.jpg",
+    "dunks.jpg",
+    "hvJerseys.jpg",
+    "abape.jpg",
+    "jordans.jpg",
+  ];
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? galleryImages.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === galleryImages.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
     <div className="hero-banner-container">
       <div className="marquee">
         {" "}
         <div className="track_02">
-          {" "}
           <img src={urlFor(image)} alt="banner" className="hero-banner-image" />
         </div>
         <div className="track_01">
           <div className="desc">
             <h2>Welcome to</h2>
-            {/* <p className="beats-solo">{smallText}</p> */}
             <h3>{midText}</h3>
             <h1>
               {largeText1}
               {largeText2}
             </h1>
-            {/* <h5>Description</h5> */}
             <p>{desc}</p>{" "}
             <Link href={`/footwear`}>
               <button type="button">{buttonText}</button>
             </Link>
           </div>
         </div>{" "}
+        <div className="track_03">
+          <div className="gallery">
+            {" "}
+            <button type="button" onClick={handlePrev}>
+              <FiChevronLeft />
+            </button>
+            <img
+              className="galleryImage"
+              src={galleryImages[currentIndex]}
+              alt={`Gallery Image ${currentIndex + 1}`}
+            />{" "}
+            <button type="button" onClick={handleNext}>
+              <FiChevronRight />
+            </button>
+          </div>
+          {/* <div className="navigation-arrows">
+        
+        
+          </div> */}
+        </div>
       </div>
     </div>
   );
